0c6ca8a56c6823b737a3fcad8031217d14403fee
hekamalutian <130629902+hekamalutian@users.noreply.github.com>
2023-05-08 21:25:19 +0330

    Add files via upload

--- SDeviation.py
+++ SDeviation.py
@@ -1,5 +1,4 @@
-import math
-
+import statistics
 def main():
     while True:
         _DATA_LIST = []
@@ -7,40 +6,23 @@ def main():
         _RERUN_PROGRAM = True
         _INDEX_OF_DATA = 0
         while (_END_OF_DATA_LIST != True):
-            _TEMP_IN = input("Enter the index " + str(_INDEX_OF_DATA) + " data [0x0=EOL]: ")
-            if ("0x0" not in _TEMP_IN and _TEMP_IN != ''):
+            _TEMP_IN = input("Enter the index " + str(_INDEX_OF_DATA) + " data [0x=EOL]: ")
+            if ("0x" not in _TEMP_IN and _TEMP_IN != ''):
                 _DATA_LIST.append(float(_TEMP_IN))
                 _INDEX_OF_DATA += 1
             elif (_TEMP_IN == ''):
                 continue
-            elif ("0x0" in _TEMP_IN and _TEMP_IN != "0x0"):
+            elif ("0x" in _TEMP_IN and _TEMP_IN != "0x"):
                 _RERUN_PROGRAM = False
                 break
-            elif (_TEMP_IN == "0x0"):
+            elif (_TEMP_IN == "0x"):
                 _END_OF_DATA_LIST = True
-
-        def _mean(_LIST):
-            _MEAN_UNDIVIDED = 0
-            for _INDEX in range(len(_LIST)):
-                _MEAN_UNDIVIDED += _LIST[_INDEX]
-            _MEAN = _MEAN_UNDIVIDED / len(_LIST)
-            return _MEAN
-        
-        def _standard_deviation(_LIST_S):
-            _NUMERATOR = 0.0
-            _MEAN_S = _mean(_LIST_S)
-            for _INDEX_S in range(len(_LIST_S)):
-                _NUMERATOR += (_LIST_S[_INDEX_S] - _MEAN_S) ** 2
-            _STANDARD_DEVIATION = math.sqrt(_NUMERATOR/len(_LIST_S))
-            return _STANDARD_DEVIATION
-        
         if _INDEX_OF_DATA != 0:
-            print("Standard Deviation: " + str(round(_standard_deviation(_DATA_LIST), 5)))
-            print("Variance of Data: " + str(round(_standard_deviation(_DATA_LIST)**2, 5)))
-            
+            print("Variance of Data: " + str(round(statistics.pvariance(_DATA_LIST), 5)))
+            print("Standard Deviation: " + str(round(statistics.pstdev(_DATA_LIST), 5)))
+            print("Coefficient of Variation: " + str(round((statistics.pstdev(_DATA_LIST)/statistics.mean(_DATA_LIST)), 5)))
         if _RERUN_PROGRAM == False:
             break
         else:
             _END_OF_DATA_LIST = False
-
 main()
